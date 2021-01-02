be95a89f53c2098c09ea030d72d231e64ff67c4e
Mikkel Paulson <git@email.mikkel.ca>
2021-01-02 13:44:14 -0500

    fix gif callback logic

--- src/image.rs
+++ src/image.rs
@@ -214,14 +214,14 @@ fn draw_weather_radar(ctx: &mut CairoRenderContext, radar_map: Vec<u8>, position
         ctx,
         &include_bytes!("../images/radar-rivers.gif")[..],
         position,
-        |decoder| {
+        |image_palette, _frame, bg_color| {
             let mut palette = HashMap::new();
 
-            for (index, color) in decoder.palette().unwrap().iter().step_by(3).enumerate() {
+            for (index, color) in image_palette.iter().step_by(3).enumerate() {
                 palette.insert(index as u8, [0x80, 0x80, 0x80, *color]);
             }
 
-            if let Some(index) = decoder.bg_color() {
+            if let Some(index) = bg_color {
                 palette.insert(index as u8, [0x00; 4]);
             }
 
@@ -229,46 +229,50 @@ fn draw_weather_radar(ctx: &mut CairoRenderContext, radar_map: Vec<u8>, position
         },
     );
 
-    draw_gif(ctx, &radar_map[..], position, |decoder| {
-        let frame = decoder.read_next_frame().unwrap().unwrap();
-        let mut scale: Vec<&u8> = Vec::new();
+    draw_gif(
+        ctx,
+        &radar_map[..],
+        position,
+        |_image_palette, frame, _bg_color| {
+            let mut scale: Vec<&u8> = Vec::new();
 
-        frame
-            .buffer
-            .iter()
-            .skip(524)
-            .step_by(frame.width as usize)
-            .for_each(|pixel| {
-                if !scale.contains(&pixel) {
-                    scale.push(pixel);
+            frame
+                .buffer
+                .iter()
+                .skip(524)
+                .step_by(frame.width as usize)
+                .for_each(|pixel| {
+                    if !scale.contains(&pixel) {
+                        scale.push(pixel);
+                    }
+                });
+
+            let mut palette = HashMap::new();
+            for i in (0x55..0xFF).step_by(0x2a) {
+                if let Some(&index) = scale.pop() {
+                    palette.insert(index, [0x00, 0x00, 0x00, i]);
                 }
-            });
-
-        let mut palette = HashMap::new();
-        for i in (0x55..0xFF).step_by(0x2a) {
-            if let Some(&index) = scale.pop() {
-                palette.insert(index, [0x00, 0x00, 0x00, i]);
             }
-        }
-        while let Some(&index) = scale.pop() {
-            palette.insert(index, [0x00, 0x00, 0x00, 0xFF]);
-        }
+            while let Some(&index) = scale.pop() {
+                palette.insert(index, [0x00, 0x00, 0x00, 0xFF]);
+            }
 
-        palette
-    });
+            palette
+        },
+    );
 
     draw_gif(
         ctx,
         &include_bytes!("../images/radar-towns.gif")[..],
         position,
-        |decoder| {
+        |image_palette, _frame, bg_color| {
             let mut palette = HashMap::new();
 
-            for (index, color) in decoder.palette().unwrap().chunks_exact(3).enumerate() {
+            for (index, color) in image_palette.chunks_exact(3).enumerate() {
                 palette.insert(index as u8, [color[0], color[1], color[2], 0xFF]);
             }
 
-            if let Some(index) = decoder.bg_color() {
+            if let Some(index) = bg_color {
                 palette.insert(index as u8, [0x00; 4]);
             }
 
@@ -277,7 +281,7 @@ fn draw_weather_radar(ctx: &mut CairoRenderContext, radar_map: Vec<u8>, position
     );
 }
 
-fn draw_gif<F: Fn(&mut gif::Decoder<&[u8]>) -> HashMap<u8, [u8; 4]>>(
+fn draw_gif<F: Fn(&[u8], &gif::Frame, Option<u8>) -> HashMap<u8, [u8; 4]>>(
     ctx: &mut CairoRenderContext,
     image: &[u8],
     position: Rect,
@@ -288,11 +292,15 @@ fn draw_gif<F: Fn(&mut gif::Decoder<&[u8]>) -> HashMap<u8, [u8; 4]>>(
 
         let mut decoder = gif::Decoder::new(image).unwrap();
         let (width, height) = (decoder.width() as usize, decoder.height() as usize);
+        let global_palette: Vec<u8> = decoder.global_palette().unwrap().iter().copied().collect();
+        let bg_color = decoder.bg_color().map(|i| i as u8);
+        let frame = decoder.read_next_frame().unwrap().unwrap();
 
-        let palette = palette_callback(&mut decoder);
+        let palette = palette_callback(&global_palette[..], &frame, bg_color);
+
         let mut buffer: Vec<u8> = Vec::with_capacity(width * height * 4);
 
-        for index in decoder.read_next_frame().unwrap().unwrap().buffer.iter() {
+        for index in frame.buffer.iter() {
             buffer.extend_from_slice(&palette.get(index).unwrap_or(&[0x00; 4])[..]);
         }
 
